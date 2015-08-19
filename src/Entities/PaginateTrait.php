9923e1d05853e970e437428793c67ae98ccfc2c7
David Rushton <david@papertank.co.uk>
2015-08-19 20:07:59 +0100

    Added getPerPage and setPerPage to PaginateTrait.

--- src/Entities/PaginateTrait.php
+++ src/Entities/PaginateTrait.php
@@ -4,6 +4,18 @@ trait PaginateTrait {
 
     protected $per_page = 20;
 
+    public function setPerPage($number)
+    {
+    	$this->per_page = $number;
+
+    	return $this;
+    }
+
+    public function getPerPage()
+    {
+    	return $this->per_page;
+    }
+
     protected function buildPaginatedResults($query, $columns = ['*'])
     {
         return $query->paginate($this->per_page, $columns);
